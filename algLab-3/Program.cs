fd0d9cdc8fd915f18f8da1f5c1e79d41bdacda70
Egoru <egor.rumyancev.9494@mail.ru>
2022-10-17 23:10:58 +0500

    Занес 3 задание

--- algLab-3/Program.cs
+++ algLab-3/Program.cs
@@ -1,4 +1,5 @@
-﻿using algLab_3.Stack;
+﻿using algLab_3.Examples;
+using algLab_3.Stack;
 using algLab_3.Tests;
 
 namespace algLab_3
@@ -7,13 +8,20 @@ namespace algLab_3
     {
         static void Main(string[] args)
         {
+
             //var tester = new Tester(1, 5000, 10);
             //tester.Testing();
             //tester.WriteCsvFile();
             // var testStr = "10 2 ^ 95 - 4 8 * +"; // 37
             //Console.WriteLine($"{testStr}: {Task4.Calculate(testStr)}");
-            var testStr = "( 7 - 10 / 2 ) * ( 8 / 4 + 4 )";
-            Console.WriteLine($"{testStr}   |   {Task5.InfixToPrefix(testStr)}");
+            //var testStr = "( 7 - 10 / 2 ) * ( 8 / 4 + 4 )";
+            //Console.WriteLine($"{testStr}   |   {Task5.InfixToPrefix(testStr)}");
+
+            //var task = new Examples.DeleteDuplicateFromLinkedList();
+            //Examples.List.ShowResult();
+            //Examples.ReverseWords.ShowResult();
+            //Examples.MaxLevelInBinaryTree.Node.ShowMaxLevel();
+            Examples.ExpressionTree.ShowExpression();
         }
     }
 }
