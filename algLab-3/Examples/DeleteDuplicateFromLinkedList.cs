69ad9838e7ee18fc7ca31bfc8f4dd310823da768
Egoru <egor.rumyancev.9494@mail.ru>
2022-10-17 23:19:10 +0500

    Теперь точно добавил

---
+++ algLab-3/Examples/DeleteDuplicateFromLinkedList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace algLab_3.Examples
+{
+
+    class List
+    {
+        Node head;
+        class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+
+        void removeDuplicates()
+        {
+            Node ptr1 = null, ptr2 = null;
+            ptr1 = head;
+
+
+            while (ptr1 != null && ptr1.next != null)
+            {
+                ptr2 = ptr1;
+
+                while (ptr2.next != null)
+                {
+
+                    if (ptr1.data == ptr2.next.data)
+                        ptr2.next = ptr2.next.next;
+                    
+                    else 
+                        ptr2 = ptr2.next;
+                }
+                ptr1 = ptr1.next;
+            }
+        }
+
+
+
+
+        void printList(Node node)
+        {
+            while (node != null)
+            {
+                Console.Write(node.data + " ");
+                node = node.next;
+            }
+        }
+
+        public static void ShowResult()
+        {
+            List list = new List();
+            list.head = new Node(10);
+            list.head.next = new Node(12);
+            list.head.next.next = new Node(11);
+            list.head.next.next.next = new Node(11);
+            list.head.next.next.next.next = new Node(12);
+            list.head.next.next.next.next.next = new Node(11);
+            list.head.next.next.next.next.next.next = new Node(10);
+
+            Console.WriteLine("Связный лист до удаления одинаковых элементов: ");
+            list.printList(list.head);
+
+            list.removeDuplicates();
+            Console.WriteLine("");
+            Console.WriteLine("Связный лист после удаления одинаковых элементов: ");
+            list.printList(list.head);
+        }
+    }
+}
---
+++ algLab-3/Examples/MaxLevelInBinaryTree.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algLab_3.Examples
+{
+    public class MaxLevelInBinaryTree
+    {
+        public class Node
+        {
+            public int data;
+            public Node left, right;
+
+            public Node(int data)
+            {
+                this.data = data;
+                this.left = null;
+                this.right = null;
+            }
+
+            static int maxLevelSum(Node root)
+            {
+                if (root == null)
+                    return 0;
+
+                int result = root.data;
+
+                Queue<Node> queue = new Queue<Node>();
+                queue.Enqueue(root);
+                while (queue.Count != 0)
+                {
+                    int count = queue.Count;
+
+                    int sum = 0;
+                    while (count-- > 0)
+                    {
+
+                        Node temp = queue.Dequeue();
+
+                        sum = sum + temp.data;
+
+                        if (temp.left != null)
+                            queue.Enqueue(temp.left);
+                        if (temp.right != null)
+                            queue.Enqueue(temp.right);
+                    }
+                    result = Math.Max(sum, result);
+                }
+                return result;
+            }
+
+
+            public static void ShowMaxLevel()
+            {
+                Node root = new Node(1);
+                root.left = new Node(2);
+                root.right = new Node(3);
+                root.left.left = new Node(4);
+                root.left.right = new Node(5);
+                root.right.right = new Node(8);
+                root.right.right.left = new Node(6);
+                root.right.right.right = new Node(7);
+
+                Console.WriteLine("Максимальная сумма равна " + maxLevelSum(root));
+            }
+        }
+    }
+}
