5a43f34fd4b843ea701cb79f8a38613c96466371
Egoru <egor.rumyancev.9494@mail.ru>
2022-10-18 23:43:33 +0500

    Добавил реализацию очереди, также второе задание

--- algLab-3/Program.cs
+++ algLab-3/Program.cs
@@ -13,18 +13,20 @@ namespace algLab_3
             //tester.WriteCsvFile();
             // var testStr = "10 2 ^ 95 - 4 8 * +"; // 37
             //Console.WriteLine($"{testStr}: {Task4.Calculate(testStr)}");
-            var memoryTester = new OutOfMemoryTester();
-            var stack = new Stack.Stack<string>();
-            var result = memoryTester.Testing(stack.Push);
-            Console.WriteLine(result.Item1);
-            Console.WriteLine(result.Item2);
-            memoryTester.GetMemoryVector().WriteCsvFile("memoryResult.csv");
-            Console.WriteLine("Запись в CSV завершена.");
+            //var memoryTester = new OutOfMemoryTester();
+            //var stack = new Stack.Stack<string>();
+            //var result = memoryTester.Testing(stack.Push);
+            //Console.WriteLine(result.Item1);
+            //Console.WriteLine(result.Item2);
+            //memoryTester.GetMemoryVector().WriteCsvFile("memoryResult.csv");
+            //Console.WriteLine("Запись в CSV завершена.");
             //var task = new Examples.DeleteDuplicateFromLinkedList();
             //Examples.List.ShowResult();
             //Examples.ReverseWords.ShowResult();
             //Examples.MaxLevelInBinaryTree.Node.ShowMaxLevel();
-            Examples.ExpressionTree.ShowExpression();
+            //Examples.ExpressionTree.ShowExpression();
+
+
         }
     }
 }
