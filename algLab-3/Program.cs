e6925b194db4383cf77b144e4064dad4b6e70313
Mikhail <m-5321e@yandex.com>
2022-10-19 02:37:04 +0500

    Корректировка метода Main

--- algLab-3/Program.cs
+++ algLab-3/Program.cs
@@ -13,18 +13,37 @@ namespace algLab_3
             //tester.WriteCsvFile();
             // var testStr = "10 2 ^ 95 - 4 8 * +"; // 37
             //Console.WriteLine($"{testStr}: {Task4.Calculate(testStr)}");
-            var memoryTester = new OutOfMemoryTester();
-            var stack = new Stack.Stack<string>();
-            var result = memoryTester.Testing(stack.Push);
-            Console.WriteLine(result.Item1);
-            Console.WriteLine(result.Item2);
-            memoryTester.GetMemoryVector().WriteCsvFile("memoryResult.csv");
-            Console.WriteLine("Запись в CSV завершена.");
+
+            //var memoryTester = new OutOfMemoryTester();
+            //var stack = new Stack.Stack<string>();
+            //var result = memoryTester.Testing(stack.Push);
+            //Console.WriteLine(result.Item1);
+            //Console.WriteLine(result.Item2);
+            //memoryTester.GetMemoryVector().WriteCsvFile("memoryResult.csv");
+            //Console.WriteLine("Запись в CSV завершена.");
+
             //var task = new Examples.DeleteDuplicateFromLinkedList();
             //Examples.List.ShowResult();
             //Examples.ReverseWords.ShowResult();
             //Examples.MaxLevelInBinaryTree.Node.ShowMaxLevel();
-            Examples.ExpressionTree.ShowExpression();
+            //Examples.ExpressionTree.ShowExpression();
+
+            var cyclicDuplexLinkedList = new Lists.CyclicDuplexLinkedList<int>();
+            cyclicDuplexLinkedList.Add(1);
+            cyclicDuplexLinkedList.Add(2);
+            cyclicDuplexLinkedList.Add(3);
+            cyclicDuplexLinkedList.Add(4);
+            cyclicDuplexLinkedList.Add(5);
+            foreach (var item in cyclicDuplexLinkedList)
+            {
+                Console.WriteLine(item);
+            }
+            cyclicDuplexLinkedList.Delete(5);
+            Console.WriteLine();
+            foreach (var item in cyclicDuplexLinkedList)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
