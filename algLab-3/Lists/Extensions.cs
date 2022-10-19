ac986a7e8722afa391c1cef5c5210c1ff6193c7b
Mikhail <m-5321e@yandex.com>
2022-10-19 15:09:38 +0500

    Добавлена часть заданий (Часть 4)

--- algLab-3/Lists/Extensions.cs
+++ algLab-3/Lists/Extensions.cs
@@ -8,6 +8,9 @@ namespace algLab_3.Lists
 {
     public static class Extensions
     {
+        /// <summary> Имя файла с тестовыми данными </summary>
+        public static string FileNameTestData { get; private set; } = "input";
+
         /// <summary>
         /// 3.	Написать функцию, которая определяет количество различных элементов списка, содержащего целые числа;
         /// Часть 4. Задание 3.
@@ -27,6 +30,27 @@ namespace algLab_3.Lists
             }
 
             return uniqueList.Count;
+        }
+
+        /// <summary> Чтение из файла списков для объединения (Часть 4. Задание 9.) </summary>
+        public static LinkedList<int> GetListFromFileTask9()
+        {
+            var lines = File.ReadAllLines($"{FileNameTestData}.txt");
+            var list1Item = lines[0].Split(' ');
+            var list2Item = lines[1].Split(' ');
+            var list1 = new LinkedList<int>();
+            var list2 = new LinkedList<int>();
+            foreach (var item in list1Item)
+            {
+                list1.Add(int.Parse(item));
+            }
+            foreach (var item in list2Item)
+            {
+                list2.Add(int.Parse(item));
+            }
+
+            list1.AddLinkedList(list2);
+            return list1;
         }
     }
 }
