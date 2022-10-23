3d0ebb302551930ba586fa198acb71fa7fc20ec1
Mikhail <m-5321e@yandex.com>
2022-10-23 06:39:04 +0500

    Auto stash before merge of "main" and "origin/main"

--- algLab-3/Program.cs
+++ algLab-3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using algLab_3.Examples;
+using algLab_3.Lists;
 using algLab_3.Stack;
 using algLab_3.Tests;
 using System.Transactions;
@@ -9,10 +10,27 @@ namespace algLab_3
     {
         static void Main(string[] args)
         {
-            //var tester = new Tester(1, 5000, 10);
-            //tester.Testing();
-            //tester.WriteCsvFile();
-            // var testStr = "10 2 ^ 95 - 4 8 * +"; // 37
+            var tester = new Tester(3, 300, 3);
+            //tester.GenerateTestFile();
+            //Thread.Sleep(5000);
+            //tester.Testing(TypeTest.Stack);
+            tester.TestingWithFixedLength(TypeTest.Queue, "15", 150); // Enqueue(elem), Print()
+            tester.TestingWithFixedLength(TypeTest.Queue, "14", 150); // Enqueue(elem),IsEmpty()
+            tester.TestingWithFixedLength(TypeTest.Queue, "45", 150); // IsEmpty(), Print()
+            tester.TestingWithFixedLength(TypeTest.Queue, "12", 150); // Enqueue(elem), Dequeue()
+
+            tester.TestingWithFixedLength(TypeTest.Queue, "152", 150); // Enqueue(elem), Print(), Dequeue()
+            tester.TestingWithFixedLength(TypeTest.Queue, "132", 150); // Enqueue(elem), First(), Dequeue()
+            tester.TestingWithFixedLength(TypeTest.Queue, "415", 150); // IsEmpty(), Enqueue(elem), Print()
+
+            tester.TestingWithFixedLength(TypeTest.Queue, "113344", 150); // Enqueue(elem), Enqueue(elem), Dequeue(), Dequeue(), IsEmpty(), IsEmpty()
+            tester.TestingWithFixedLength(TypeTest.Queue, "114455", 150); // Enqueue(elem), Enqueue(elem), IsEmpty(), IsEmpty(), Print(), Print()
+            tester.TestingWithFixedLength(TypeTest.Queue, "444111", 150); // IsEmpty(), IsEmpty(), IsEmpty(), Enqueue(elem), Enqueue(elem), Enqueue(elem)
+
+            tester.GetTimeVector().WriteCsvFile();
+
+
+            //var testStr = "10 2 ^ 95 - 4 8 * +"; // 37
             //Console.WriteLine($"{testStr}: {Task4.Calculate(testStr)}");
 
             //var memoryTester = new OutOfMemoryTester();
